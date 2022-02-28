0f871b1ffc7d156d35e015dab96bca3d1d3a65ea
Ali Sal <ali@moveaheadmedia.co.uk>
2022-02-28 16:45:05 +0700

    Code optimization

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '0.3.1',
+        'version' => '0.3.1.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'wpstrap/wordpress-plugin-boilerplate',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'micropackage/internationalization' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../micropackage/internationalization',
+            'aliases' => array(),
+            'reference' => '8de158b8fc71557a8310f0d98d2b8ad9f4f44dd5',
+            'dev_requirement' => false,
+        ),
+        'micropackage/requirements' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../micropackage/requirements',
+            'aliases' => array(),
+            'reference' => '114d49df7c5860108c008853b514d9a7eccaedf3',
+            'dev_requirement' => false,
+        ),
+        'wpstrap/wordpress-plugin-boilerplate' => array(
+            'pretty_version' => '0.3.1',
+            'version' => '0.3.1.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
